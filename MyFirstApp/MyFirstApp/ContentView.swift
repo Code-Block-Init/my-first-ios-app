b516caff976b7ddcd25064147f969a5065371c2f
Aswini S <ashumeow@live.com>
2019-10-05 19:00:22 +0530

    Add files via upload

---
+++ MyFirstApp/MyFirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by aswini sridhar on 10/5/19.
+//  Copyright © 2019 aswini sridhar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
